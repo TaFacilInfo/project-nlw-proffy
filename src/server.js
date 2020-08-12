ac7b25dc45ee49f5661a7c236b2bb9372c29b512
unknown <injex@T102.grupoinjex.net>
2020-08-11 22:08:46 -0300

    Projeto Proffy - NLW - JS

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
+// Servidor
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
-.get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
+
+// Configurar nunjucks (Template Engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
-.get("/study", (req, res) => {
-    return res.send("study")
-})
+
+// Início e configuração do servidor
+server
+// Receber os dados do req.body
+.use(express.urlencoded({ extended: true }))
+// Configura arquivos estáticos  (css, scripts, imagens)
+.use(express.static("public"))
+// Rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+// Start do servidor
 .listen(5500)
